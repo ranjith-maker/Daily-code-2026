ba8751c43618c4569580bc172d7b3bf994de25d8
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2026-03-17 22:44:27 +0530

    github profile viewer

---
+++ React/Day 08 - github profile viewer - API/Main.js
@@ -0,0 +1,45 @@
+import React , {useState, useEffect,useCallback }  from "react"
+import ReactDOM from "react-dom/client"
+import Header from "./src/components/Header"
+import Card from "./src/components/Card"
+
+function GithubProfile() {
+
+return (
+
+<>
+
+<Header></Header>
+<Card></Card>
+
+</>
+
+)}
+
+let road = ReactDOM.createRoot(document.getElementById("road"))
+
+road.render(<GithubProfile/> )
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+
