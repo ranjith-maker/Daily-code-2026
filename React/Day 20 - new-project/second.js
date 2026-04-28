22cbf877b18944a9922411a5982c6d1672f4c88c
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2026-04-28 22:01:39 +0530

    routing

---
+++ React/Day 20 - new-project/second.js
@@ -0,0 +1,48 @@
+// import React from 'react'
+// import Header from '../Components/Header'
+// import Body from '../Components/Body'
+// import About from '../Components/About'
+// import Contact from '../Components/Contact'
+// import { createBrowserRouter , RouterProvider   } from 'react-router-dom'
+// import Layout from '../Components/Layout'
+// import Error from '../Components/Error'
+// import Menu from '../Components/Menu'
+
+// const appRouter = createBrowserRouter([
+// {
+// path : "/",
+// element : <Layout/>,
+// children : [
+// {
+// index: true,
+// element : <Body/>
+// },
+
+// {
+// path : "/about",
+// element : <About/>
+// },
+
+// {
+// path : "/contact" ,
+// element : <Contact/> 
+// },
+
+// {
+// path : "/restaurants/:id",
+// element : <Menu/>
+//  }
+
+
+
+// ],
+// errorElement : <Error/>    , 
+// },
+
+// ])
+
+
+// export default function App() {
+//   return <RouterProvider  router={appRouter}   />
+// }
+
