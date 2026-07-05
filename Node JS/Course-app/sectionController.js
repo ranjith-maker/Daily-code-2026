1f0864fef256f6d11939b685cd72475508b2b031
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2026-07-05 22:16:43 +0530

    course-07

--- Node JS/Course-app/sectionController.js
+++ Node JS/Course-app/sectionController.js
@@ -52,3 +52,102 @@ return res.status(201).json({
 
 
 
+export const updateSection = catchAsync(async(req,res,next)=>{
+
+    const { sectionName, sectionId } = req.body;
+
+    if(!sectionName || !sectionId){
+        throw new AppError("Invalid fields",400);
+    }
+
+    const section = await Section.findById(sectionId);
+
+
+    if(!section){
+        throw new AppError("Section not found",404);
+    }
+
+
+//Find another section with this name, but not the one I am currently updating.
+    const duplicate = await Section.findOne({ course: section.course,
+        sectionName: sectionName.trim(),
+        _id:{ $ne: sectionId  }
+    })
+
+
+    if(duplicate){
+        throw new AppError("Section with this name already exists",409);
+    }
+
+    const updatedSection = await Section.findByIdAndUpdate(sectionId,
+        {sectionName: sectionName.trim() },  {new:true}
+    );
+
+    return res.status(200).json({
+        success:true,
+        message:'Section name updated successfully',
+        data:updatedSection
+    });
+
+})
+
+
+
+export const deleteSection = catchAsync(async(req,res,next)=>{
+
+const {sectionId} = req.params
+
+if(!sectionId){
+    throw new AppError('Section ID is not valid',400)
+}
+
+const section = await Section.findById(sectionId)
+
+if(!section){
+    throw new AppError('Section ID not found',404)
+}
+
+// Find SubSection documents where _id exists inside this array
+const deleteSubSection = await SubSection.deleteMany({
+        _id: {
+            $in: section.subSection
+        }
+    });
+    
+
+await Course.updateMany({courseContent: sectionId},
+    { $pull: {
+            courseContent: sectionId
+        }
+    }
+);
+
+
+await Section.findByIdAndDelete(sectionId);
+
+return res.status(200).json({
+    success : true,
+    message : 'Section Deleted Successfully'
+})
+
+
+})
+
+
+
+
+/*
+whenever we delete or and wherever ref exist
+we need to delete from all of it right ?
+if what we're deleting is just a field in another model then just make updateMany pull it deleted
+if where we're deleting inside we've another object ID like subsection then we need to use id :{$in} to delete it contents too as we here delete the ids
+
+
+We use populate() in Mongoose to replace stored ObjectIds with the actual documents they refer to.
+MongoDB does not automatically join collections like SQL. It stores references only.
+Because subSection doesn't belong to the Course model.
+It belongs to the Section model.
+Populate courseContent first. Inside each populated courseContent document, also populate subSection.
+That's why subSection is inside the object.
+*/
+
