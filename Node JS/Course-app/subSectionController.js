95ca01e73b08bf2ca1a5b9c4df3a8aee3e22edd8
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2026-07-07 21:11:09 +0530

    course-09

--- Node JS/Course-app/subSectionController.js
+++ Node JS/Course-app/subSectionController.js
@@ -57,3 +57,97 @@ return res.status(201).json({
 
 })
 
+
+export const updateSubSection = catchAsync(async(req,res,next)=>{
+
+const {subSectionId} = req.params
+const {title,timeDuration,description,videoURL} = req.body
+
+if(!subSectionId || !title || !timeDuration || !description || !videoURL){
+    throw new AppError('Invalid details',400)
+}
+
+const subSection = await SubSection.findById(subSectionId)
+
+if(!subSection){
+    throw new AppError('Subsection is not found',404)
+}
+
+Object.keys(req.body).forEach((key)=>{
+    subSection[key] = req.body[key]
+})
+
+await subSection.save()
+
+
+return res.status(200).json({
+    success : true,
+    message : 'Subsection Edited Successfully',
+    data: subSection   
+})
+
+
+})
+
+export const deleteSubSection = catchAsync(async(req,res,next)=>{
+
+const {subSectionId} = req.params
+
+if(!subSectionId){
+    throw new AppError('Subsection ID is not valid',400)
+}
+
+const subSection = await SubSection.findById(subSectionId)
+
+if(!subSection){
+    throw new AppError('Subsection not found',404)
+}
+
+
+const section = await Section.findOne({ subSection : subSectionId  })
+
+if(!section){
+    throw new AppError('No subsection is found',404)
+}
+
+await Section.findByIdAndUpdate( section._id ,
+   
+{ $pull : { subSection : subSectionId  } }  )
+
+await SubSection.findByIdAndDelete(subSectionId)
+
+
+return res.status(200).json({
+    success : true,
+    message : 'Subsection is deleted successfully'
+})
+
+
+})
+
+
+
+
+
+/**
+ * if we 're deleting a children inside a parent you first and both are diff schemas
+ * first get childID verify now try to get engage parent with child as
+ * Section.findOne({subSection : sectionId(params)}) 
+ * so on updateMany  inside pull the child from the parent
+ * now delete the subSection
+ * 
+ * 
+ * 
+ * 
+const existingSubSection = await Section.findOne( {subSection : sectionId})
+
+if(existingSubSection){
+    throw new AppError(' This subsection is alredy created ',400)
+}
+
+ * 
+ * 
+ * 
+ */
+
+
