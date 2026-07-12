eabef9c4b9cee0c58d775dd8e7ad7fc43e33bc1c
snranjith500@gmail.com <snranjithkumar500@gmail.com>
2026-07-12 21:50:01 +0530

    course-12

--- Node JS/Course-app/Errors.js
+++ Node JS/Course-app/Errors.js
@@ -63,4 +63,100 @@ Wishlist
 Notifications
 Certificates
 
+
+
+
+
+Database
+   |
+   |
+Conversation.find()
+   |
+   ↓
+Get conversations containing me
+
+[
+  {
+    participants: [Me, Sarah]
+  },
+  {
+    participants: [Me, Alex]
+  }
+]
+
+   |
+   |
+map()
+
+   |
+   ↓
+
+[
+  {
+    recipient: Sarah
+  },
+  {
+    recipient: Alex
+  }
+]
+
+
+
+
+
+
+
+
+
+
+
+1. User enters:
+   - Username
+   - Email
+
+2. Server validates:
+   - Username is available
+   - Email isn't already registered
+
+5. Save to OTP collection:
+
+{
+    username,
+    email,
+    otp: hashedOTP,
+    createdAt
+}
+
+6. Send OTP email
+
+7. User enters OTP
+
+8. Verify OTP
+
+9. Create User:
+
+{
+    username,
+    email,
+    profilePicture: "",
+    bio: ""
+}
+
+10. Delete OTP document
+
+11. Generate JWT
+
+So yes, your reasoning is exactly right:
+when user disconnects we dont wanna show the user as online thas why we wrote in disconnect right ?
+Exactly. If you didn't emit the updated list after deleting the user, every client would still think that user was online.
+
+Why not emit only once?
+Because the list changes in both cases:
+•	✅ A user connects → add them → notify everyone. 
+•	✅ A user disconnects → remove them → notify everyone. 
+Whenever the data changes, you broadcast the new version.
+
+
+
+
 */
